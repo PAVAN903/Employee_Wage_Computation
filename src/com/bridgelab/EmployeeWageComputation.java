372074625120a0a7a5c612d9f85998b55b719249
PAVAN903 <navap903@gmail.com>
2023-03-04 22:20:20 +0530

    Added:swtch case

--- src/com/bridgelab/EmployeeWageComputation.java
+++ src/com/bridgelab/EmployeeWageComputation.java
@@ -3,28 +3,32 @@ package com.bridgelab;
 import java.util.Random;
 
 public class EmployeeWageComputation {
-
 	public static void main(String[] args) {
-		Random ran=new Random();
-		int FullTime=0;
-		int empHr=0;
-		int wagePerHour=20;
-		int PartTime=2;
-		int Attendance=ran.nextInt(3);
-		if(Attendance==FullTime) {
+		Random ran = new Random();
+		int FullTime = 0;
+		int empHr = 0;
+		int wagePerHour = 20;
+
+		int Attendance = ran.nextInt(3);
+
+		switch (Attendance) {
+		case 1:
 			System.out.println("Employee is Present");
-			empHr=8;
-			int dailyWage=empHr*wagePerHour;
-			System.out.println("Employee daily wage:: "+dailyWage);
-			}else if(Attendance==PartTime) {
-				System.out.println("Employee is Part Time");
-				empHr=4;
-				int dailyWage=empHr*wagePerHour;
-				System.out.println("Employee part time daily wage:: "+dailyWage);
-			}else {
+			empHr = 8;
+			int dailyWage = empHr * wagePerHour;
+			System.out.println("Employee daily wage:: " + dailyWage);
+			break;
+		case 2:
+			System.out.println("Employee is Part Time");
+			empHr = 4;
+			int dailyWagePart = empHr * wagePerHour;
+			System.out.println("Employee part time daily wage:: " + dailyWagePart);
+			break;
+		default:
 			System.out.println("Employee is Absent");
-			empHr=0;
+			empHr = 0;
+			break;
+
 		}
 	}
-
 }
